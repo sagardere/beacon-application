2fd01ce24c3a63014518d9ab314bb5c3f43747e5
Ruchika <ruchikapawar7@gmail.com>
2019-02-21 19:33:36 +0530

    add changes

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -7,6 +7,7 @@ let Beacon = models.beacon();
 const async = require('async');
 var ObjectId = require('mongoose').Types.ObjectId;
 var helper = require('../lib/helper')();
+//var convertTime = require('convert-time');
 module.exports = () => {
   var result = {};
   
@@ -70,6 +71,8 @@ module.exports = () => {
 //****************************************************************************************
   //Post
   result.newCampaigns = async (req, res, next) => {
+     var startTime;
+     var endTime;
     try {
       if (!req.body || !req.body.advertisementId) {
         throw new Error('advertisementId not defined.');
@@ -80,16 +83,6 @@ module.exports = () => {
       if (!req.body || !req.body.campaignTitle) {
         throw new Error('campaignTitle not defined.');
       }
-      // if (!req.body || !req.body.startDate) {
-      //   throw new Error('startDate not defined.');
-      // }
-      // if (!req.body || !req.body.endDate) {
-      //   throw new Error('endDate not defined.');
-      // }
-
-        console.log(req.body.startDate);
-        console.log(req.body.endDate);
-      
       
       if((req.body.startDate != "") && (req.body.startDate != undefined)){
           var startDate = helper.stringToDate(req.body.startDate);
@@ -100,21 +93,35 @@ module.exports = () => {
           var endDate = helper.stringToDate(req.body.endDate);
           //console.log(endDate);
       }
-      if (!req.body || !req.body.targetAge) {
-        throw new Error('targetAge not defined.');
+
+       if((req.body.startTime != "") && (req.body.startTime != undefined)){
+        console.log("in if part")
+       startTime = new Date(req.body.startDate);
+      console.log(startTime);
+      // console.log(startTime);
+      startTime.setHours(parseInt(req.body.startTime));
+      console.log(startTime)
       }
-     
+
+      if((req.body.endTime != "") && (req.body.endTime != undefined)){
+        console.log("in if part")
+       endTime = new Date(req.body.endDate);
+      console.log(endTime);
+      // console.log(startTime);
+      endTime.setHours(parseInt(req.body.endTime));
+      }
+      
+      // console.log(startTime)
+      // console.log(endTime)
+
       let userId = req.body.id || '';
       let advertisementId = req.body.advertisementId || '';
       let beaconId = req.body.beaconId || '';
       let campaignTitle = req.body.campaignTitle || '';
-      let startTime = req.body.startTime || '';
-      let endTime = req.body.endTime || '';
-      // let startDate = req.body.startDate || '';
-      // let endDate = req.body.endDate || '';
       let daysOfWeek = req.body.daysOfWeek || '';
       let gender = req.body.gender || '';
-      let targetAge = req.body.targetAge || '';
+      let minage = req.body.minage || '';
+      let maxage = req.body.maxage || ''
       let status = req.body.status || '';
       let campaign = new Campaign({
         userId: userId,
@@ -129,10 +136,14 @@ module.exports = () => {
           daysOfWeek: daysOfWeek
         },
         gender: gender,
-        targetAge: targetAge,
+        targetAge: {
+          minage:minage,
+          maxage:maxage
+        },
         status: status
       });
       
+      console.log(campaign)
       let newCampaign = await campaign.save();
       if (!newCampaign) throw new Error('Error in campaign saving.');
       res.json({
@@ -147,8 +158,145 @@ module.exports = () => {
       })
     }
   }
-// ************************************************************************************
+// ***************************************************************************
+result.campaignDetails = async(req, res, next)=>{
+  console.log('Inside campaignDetails');
+  try{
+
+     if (!req.body || !req.body.campaignId) {
+        throw new Error('campaignId not defined.');
+      }
+      let campaignId = req.body.campaignId;
+      
+      let campaignData = await Campaign.find({
+            _id: campaignId
+          });
+     
+      if(!campaignData) campaignData= '';
+      res.json({
+          success: true,
+          data: campaignData
+        });
+
+  }catch (err) {
+      return res.json({
+        success: false,
+        message: "Error in getting Campaign Details.."
+      })
+    }
+}
+//****************************************************************************************
+// result.updateCampaign = async (req, res, next)=>{
+//      console.log('Inside updateCampaign');
+//      try{
+
+//       if (!req.body || !req.body.campaignId) {
+//         throw new Error('campaignId not defined.');
+//       }
+//       // if (!req.body || !req.body.advertisementId) {
+//       //   throw new Error('advertisementId not defined.');
+//       // }
+//       // if (!req.body || !req.body.beaconId) {
+//       //   throw new Error('beaconId not defined.');
+//       // }
+//       // if (!req.body || !req.body.campaignTitle) {
+//       //   throw new Error('campaignTitle not defined.');
+//       // }
+
+//       // if (!req.body || !req.body.startTime) {
+//       //   throw new Error('startTime not defined.');
+//       // }
+
+//       // if (!req.body || !req.body.endTime) {
+//       //   throw new Error('endTime not defined.');
+//       // }
+
+//       // if (!req.body || !req.body.startDate) {
+//       //   throw new Error('startDate not defined.');
+//       // }
+
+//       // if (!req.body || !req.body.endDate) {
+//       //   throw new Error('endDate not defined.');
+//       // }
+
+//       // if (!req.body || !req.body.daysOfWeek) {
+//       //   throw new Error('daysOfWeek not defined.');
+//       // }
+
+//       // if (!req.body || !req.body.gender) {
+//       //   throw new Error('gender not defined.');
+//       // }
+
+//       // if (!req.body || !req.body.minage) {
+//       //   throw new Error('minage not defined.');
+//       // }
+
+//       // if (!req.body || !req.body.maxage) {
+//       //   throw new Error('maxage not defined.');
+//       // }
+
+//       // if (!req.body || !req.body.status) {
+//       //   throw new Error('status not defined.');
+//       // }
+
+//       let campaignId = req.body.campaignId;
+//       let advertisementId = req.body.advertisementId || '';
+//       let beaconId = req.body.beaconId || '';
+//       let campaignTitle = req.body.campaignTitle || '';
+//       let startTime = req.body.startTime || '';
+//       let endTime = req.body.endTime || '';
+//       let startDate = req.body.startDate || '';
+//       let endDate = req.body.endDate || '';
+//       let daysOfWeek = req.body.daysOfWeek || '';
+//       let gender = req.body.gender || '';
+//       let minage = req.body.minage || '';
+//       let maxage = req.body.maxage || '';
+//       let status = req.body.status || '';
+
+//       let obj = {
+//       advertisementId:advertisementId,
+//       beaconId:beaconId,
+//       campaignTitle:campaignTitle,
+//       schedule:{
+//       startTime:startTime,
+//       endTime:endTime,
+//       startDate:startDate,
+//       endDate:endDate,
+//       daysOfWeek:daysOfWeek
+//     },
+//       gender:gender,
+//       targetAge:{
+//       minage:minage,
+//       maxage:maxage
+//     },
+//       status:status
+
+//     }
+//     let query = {
+//       $set: obj
+//     }
+
+//     let updateCampaign = await Campaign.findOneAndUpdate({
+//                     _id : campaignId
+//                   },query,{new: true});
+    
+//         if (!updateCampaign) throw new Error('Error in updating campaign..');
+//         res.json({
+//           success:true,
+//           message:'Campaign updated...',
+//           data:updateCampaign
+//          });
+
+//      }catch (err) {
+//       return res.json({
+//         success: false,
+//         message: err.toString()
+//       })
+//     }
+// }
+//****************************************************************************************
   result.assignBeacon = async (req, res, next) => {
+    console.log('Inside assignBeacon');
     try {
       if (!req.body || !req.body.campaignId) {
         throw new Error('campaignId not defined...');
@@ -159,24 +307,60 @@ module.exports = () => {
       let campaignId = req.body.campaignId;
       let beaconId = req.body.beaconId;
 
-      Campaign.findOne({
-        _id: campaignId
-      },function(err,result){
-        if(err){ 
-          res.send("Campaign not found");
-        } 
-        else {
-        result.beaconId = beaconId;
-        //console.log(result);
-        result.save();
+      // Campaign.findOne({
+      //   _id: campaignId
+      // },{upsert:true},function(err,result){
+      //   if(err){ 
+      //     res.send("Campaign not found");
+      //   } 
+      //   else {
+      //   result.beaconId = beaconId;
+      //   //console.log(result);
+      //   result.save();
+      //   res.json({
+      //     success: true,
+      //     message: 'Beacon assigned...',
+      //     data: {beaconId:result.beaconId}
+      //   });
+      //   }
+
+      //  })
+      let obj ={
+        "beaconId":beaconId
+      }
+
+      let query = {
+      $set: obj
+      }
+
+      let updateCampaign = await Campaign.findOneAndUpdate({
+                    "_id" : campaignId
+                  },query,{new: true});
+      console.log(updateCampaign);
+        if (!updateCampaign) throw new Error('Error in updating campaign...');
         res.json({
-          success: true,
-          message: 'Beacon assigned...',
-          data: {beaconId:result.beaconId}
-        });
-        }
-        
-      })
+          success:true,
+          message:'Beacon assigned...',
+          data:{beaconId:updateCampaign.beaconId}
+         });
+
+
+
+
+      // Campaign.findOneAndUpdate({ _id: campaignId},{$set:{beaconId:beaconId}},
+      // {new:true}).then((err,b1)=>{
+      //   if(err){
+      //     res.send(err)
+      //   }
+      //   else{
+      //     res.json({
+      //     success: true,
+      //     message: 'Beacon assigned...',
+      //     data: {beaconId:b1.beaconId}
+      //   });
+      //   }
+      // })
+
     } catch (err) {
       return res.json({
         success: false,
